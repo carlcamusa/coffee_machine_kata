6cc553383e165691bf30c1422d641fa2238213a0
carlcamusa <carlcamusa@gmail.com>
2022-02-22 17:48:03 +0000

    OrderLogic now handles coffee and hot chocolate orders too

--- src/coffee_machine/customer_order.py
+++ src/coffee_machine/customer_order.py
@@ -3,6 +3,8 @@ from enum import Enum
 
 class DrinkType(Enum):
     TEA = 0
+    COFFEE = 1
+    CHOCOLATE = 2
 
 
 class CustomerOrder:
@@ -12,5 +14,9 @@ class CustomerOrder:
     def command(self):
         if self.drink == DrinkType.TEA:
             return "T"
+        elif self.drink == DrinkType.COFFEE:
+            return "C"
+        elif self.drink == DrinkType.CHOCOLATE:
+            return "H"
         else:
             return ""
--- tests/test_customer_order.py
+++ tests/test_customer_order.py
@@ -5,3 +5,15 @@ def test_a_tea_order_command():
     an_order = CustomerOrder(DrinkType.TEA)
 
     assert an_order.command() == "T"
+
+
+def test_a_coffee_order_command():
+    an_order = CustomerOrder(DrinkType.COFFEE)
+
+    assert an_order.command() == "C"
+
+
+def test_a_hot_chocolate_order_command():
+    an_order = CustomerOrder(DrinkType.CHOCOLATE)
+
+    assert an_order.command() == "H"
