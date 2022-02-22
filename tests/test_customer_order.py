285c7f3550df807409d313d59207bb54d37fdb68
carlcamusa <carlcamusa@gmail.com>
2022-02-22 18:24:03 +0000

    add missing quiz to CustomerOrder to check orders with sugar

--- tests/test_customer_order.py
+++ tests/test_customer_order.py
@@ -1,6 +1,12 @@
 from coffee_machine.customer_order import CustomerOrderFactory, DrinkType
+from coffee_machine.sugar_quantity import SugarQuantityType
 
 
 def test_a_tea_order_drink_code():
     customer_order = CustomerOrderFactory.get(DrinkType.TEA)
     assert customer_order.drink_code() == "T"
+
+
+def test_a_single_sugar_order_sugar_code():
+    customer_order = CustomerOrderFactory.get(DrinkType.TEA, SugarQuantityType.SINGLE)
+    assert customer_order.sugar_code() == "1"
