b0eb6a86664bd3af91e44ba4b44e9a726dafb373
carlcamusa <carlcamusa@gmail.com>
2022-03-01 16:32:19 +0100

    refactor: pass the order as an argument to the process_order method instead of on OrderLogic class initialization

--- src/coffee_machine/order_logic.py
+++ src/coffee_machine/order_logic.py
@@ -4,22 +4,21 @@ from coffee_machine.drink_maker import DrinkMaker
 
 
 class OrderLogic:
-    def __init__(self, order: CustomerOrder, drink_maker: DrinkMaker, credit_checker: CreditChecker):
-        self.order = order
+    def __init__(self, drink_maker: DrinkMaker, credit_checker: CreditChecker):
         self.drink_maker = drink_maker
         self.credit_checker = credit_checker
 
-    def process_order(self) -> None:
-        if self.credit_checker.enough_credits_available_for(self.order.cost()):
-            command = self.command_for_order()
+    def process_order(self, order: CustomerOrder) -> None:
+        if self.credit_checker.enough_credits_available_for(order.cost()):
+            command = self.command_for_order(order)
         else:
-            pending_credits = self.credit_checker.pending_amount_to(self.order.cost())
+            pending_credits = self.credit_checker.pending_amount_to(order.cost())
             command = self.command_for_message(pending_credits)
 
         self.drink_maker.set_command(command)
 
-    def command_for_order(self):
-        return f"{self.order.drink_code()}:{self.order.sugar_code()}:{self.order.stick_code()}"
+    def command_for_order(self, order: CustomerOrder):
+        return f"{order.drink_code()}:{order.sugar_code()}:{order.stick_code()}"
 
     @staticmethod
     def command_for_message(message: str):
--- tests/test_order_logic.py
+++ tests/test_order_logic.py
@@ -9,46 +9,46 @@ from coffee_machine.order_logic import OrderLogic
 
 
 def test_generates_a_command_for_a_tea_order():
-    an_order = CustomerOrderFactory.get(DrinkType.TEA)
     drink_maker_fake = None
     credit_checker_fake = None
-    order_logic = OrderLogic(an_order, drink_maker_fake, credit_checker_fake)
+    order_logic = OrderLogic(drink_maker_fake, credit_checker_fake)
+    an_order = CustomerOrderFactory.get(DrinkType.TEA)
 
-    assert order_logic.command_for_order() == f"T::"
+    assert order_logic.command_for_order(an_order) == f"T::"
 
 
 def test_generates_a_command_for_a_single_sugar_with_a_stick_order():
+    drink_maker_fake = None
+    credit_checker_fake = None
     an_order = CustomerOrderFactory.get(
         DrinkType.TEA,
         SugarQuantityType.SINGLE
     )
-    drink_maker_fake = None
-    credit_checker_fake = None
-    order_logic = OrderLogic(an_order, drink_maker_fake, credit_checker_fake)
+    order_logic = OrderLogic(drink_maker_fake, credit_checker_fake)
 
-    assert order_logic.command_for_order() == f"T:1:0"
+    assert order_logic.command_for_order(an_order) == f"T:1:0"
 
 
 def test_generates_a_command_for_a_double_sugar_with_a_stick_order():
+    drink_maker_fake = None
+    credit_checker_fake = None
     an_order = CustomerOrderFactory.get(
         DrinkType.COFFEE,
         SugarQuantityType.DOUBLE
     )
-    drink_maker_fake = None
-    credit_checker_fake = None
-    order_logic = OrderLogic(an_order, drink_maker_fake, credit_checker_fake)
+    order_logic = OrderLogic(drink_maker_fake, credit_checker_fake)
 
-    assert order_logic.command_for_order() == f"C:2:0"
+    assert order_logic.command_for_order(an_order) == f"C:2:0"
 
 
 def test_processes_an_order():
-    an_order = CustomerOrderFactory.get(DrinkType.TEA)
     drink_maker_spy = Spy(DrinkMaker)
     with Stub(CreditChecker) as credit_checker_stub:
         credit_checker_stub.enough_credits_available_for(ANY_ARG).returns(True)
-    order_logic = OrderLogic(an_order, drink_maker_spy, credit_checker_stub)
+    order_logic = OrderLogic(drink_maker_spy, credit_checker_stub)
+    an_order = CustomerOrderFactory.get(DrinkType.TEA)
 
-    order_logic.process_order()
+    order_logic.process_order(an_order)
 
     assert_that(drink_maker_spy.set_command, called().with_args("T::"))
 
@@ -60,33 +60,33 @@ def test_generates_a_message_command():
 
 
 def test_sends_a_message_with_the_pending_amount_when_there_is_not_enough_credit_available():
-    an_order = CustomerOrderFactory.get(
-        DrinkType.TEA,
-        SugarQuantityType.NONE
-    )
     drink_maker_spy = Spy(DrinkMaker)
     a_pending_amount = randint(1, 10)
     with Stub(CreditChecker) as credit_checker_stub:
         credit_checker_stub.enough_credits_available_for(ANY_ARG).returns(False)
         credit_checker_stub.pending_amount_to(ANY_ARG).returns(a_pending_amount)
-    order_logic = OrderLogic(an_order, drink_maker_spy, credit_checker_stub)
+    order_logic = OrderLogic(drink_maker_spy, credit_checker_stub)
+    an_order = CustomerOrderFactory.get(
+        DrinkType.TEA,
+        SugarQuantityType.NONE
+    )
 
-    order_logic.process_order()
+    order_logic.process_order(an_order)
 
     assert_that(drink_maker_spy.set_command, called().with_args(f"M:{a_pending_amount}"))
 
 
 def test_integration_sends_a_message_with_the_pending_amount_when_there_is_not_enough_credit_available():
+    drink_maker_spy = Spy(DrinkMaker)
+    available_credits = 0.1
+    credit_checker = CreditChecker(available_credits)
+    order_logic = OrderLogic(drink_maker_spy, credit_checker)
     an_order = CustomerOrderFactory.get(
         DrinkType.TEA,
         SugarQuantityType.NONE
     )
-    drink_maker_spy = Spy(DrinkMaker)
-    available_credits = 0.1
-    credit_checker = CreditChecker(available_credits)
     pending_amount = an_order.cost() - available_credits
-    order_logic = OrderLogic(an_order, drink_maker_spy, credit_checker)
 
-    order_logic.process_order()
+    order_logic.process_order(an_order)
 
     assert_that(drink_maker_spy.set_command, called().with_args(f"M:{pending_amount}"))
